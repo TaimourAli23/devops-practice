5745556610242f0cb9b796c19a54746c2df64a90
Taimour Ali <133780034+TaimourAli23@users.noreply.github.com>
2024-09-11 12:04:59 +0500

    Envrionment based deployment

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -131,8 +131,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME','postgres'),
         'USER': os.getenv('DB_USER','postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'admin'),
-        'HOST': os.getenv('DB_HOST','localhost'),
-        'PORT': os.getenv('DB_PORT','5433'),
+        'HOST': os.getenv('DB_HOST','postgresdb4'),
+        'PORT': os.getenv('DB_PORT','5432'),
     }
 }
 
