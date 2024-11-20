8a6647a14cd71a1db371f305eb73aefdce0fa6aa
Taimour Ali <133780034+TaimourAli23@users.noreply.github.com>
2024-11-20 16:31:55 +0500

    terraform scripts

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -10,10 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
 import os
-
-from django.conf.global_settings import CSRF_TRUSTED_ORIGINS
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
